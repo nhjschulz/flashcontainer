da2536f0ac4a134297232d1f604fae522b5b4b87
Haju Schulz <haju.schulz@online.de>
2023-04-02 21:18:45 +0200

    tc3xx: fix cmd banner
    it generates pargen files not hex.

--- src/flashcontainer/tc3xx.py
+++ src/flashcontainer/tc3xx.py
@@ -48,7 +48,7 @@ def tc3xx(argv) -> int:
     logging.basicConfig(encoding='utf-8', level=logging.WARNING)
 
     name = "tc3xx"
-    about = f"{name} {__version__}: Build hex files for TC3XX data structures."
+    about = f"{name} {__version__}: Build pargen definitions for TC3xx data structures."
 
     parser = FileArgumentParser(
         prog=name,
